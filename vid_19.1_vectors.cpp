0628cc8b73d1a33743381d0c71134cc318aa4358
Ryder-07 <kunals1209@gmail.com>
2024-10-02 19:30:39 +0530

    learnt vectors

--- vid_19.1_vectors.cpp
+++ vid_19.1_vectors.cpp
@@ -1,37 +1,68 @@
-#include <iostream>
-#include <vector>
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	vector<int> v;
+//	
+//	v.push_back(1);
+//	v.push_back(2);
+//	v.push_back(3);
+//	
+//	for(int i=0;i<v.size();i++){
+//		cout<<v[i]<<endl;
+//	}
+//	vector<int>::iterator it;
+//	for(it=v.begin();it < v.end();it++){
+//		cout<<*it<<endl;
+//	}
+////	for(auto element:v){
+////		cout<<element<<endl;
+////	}
+//	
+//	v.pop_back();
+//	
+//	vector<int> v2 (3,50);
+//	
+//	swap(v,v2); 
+//	
+//	for(it=v.begin();it < v.end();it++){
+//		cout<<*it<<endl;
+//	}
+//	for(it=v2.begin();it < v2.end();it++){
+//		cout<<*it<<endl;
+//	}
+//}
+
+#include<iostream>
+#include<vector>
 
 using namespace std;
 
-int main()
-{
-	vector<int> v;
+bool myCompare(pair<int,int> p1,pair<int,int> p2){
+	return p1.first<p2.first;
+}
+
+
+int main(){
+	int arr[]={10,16,7,14,5,3,2,9};
+	vector<pair <int, int> > v;
 	
-	v.push_back(1);
-	v.push_back(2);
-	v.push_back(3);
+	for(int i=0;i<(sizeof(arr)/sizeof(arr[0]));i++){
+		v.push_back(make_pair(arr[i],i));
+	}
+	
+	sort(v.begin() ,v.end() ,myCompare);
 	
 	for(int i=0;i<v.size();i++){
-		cout<<v[i]<<endl;
+		arr[v[i].second]=i;
 	}
-	vector<int>::iterator it;
-	for(it=v.begin();it < v.end();it++){
-		cout<<*it<<endl;
+	for(int i=0;i<v.size();i++){
+		cout<<arr[i];
 	}
-//	for(auto element:v){
-//		cout<<element<<endl;
-//	}
-	
-	v.pop_back();
-	
-	vector<int> v2 (3,50);
-	
-	swap(v,v2); 
-	
-	for(it=v.begin();it < v.end();it++){
-		cout<<*it<<endl;
-	}
-	for(it=v2.begin();it < v2.end();it++){
-		cout<<*it<<endl;
-	}
+	return 0;
 }
+
+
